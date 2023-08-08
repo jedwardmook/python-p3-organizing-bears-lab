3107819c71f05f7f11508700c47c7b84a5fa4ff1
JedwardMook <jedwardmook@yahoo.com>
2023-08-08 13:18:47 -0500

    All tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "Chocolate", "Chill", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 9, "F", "White", "Rowdy", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 1, "F", "White", "Calm", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 5, "M", "Brown", "Authoritative", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 5, "F", "Black", "Sweet", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 14, "M", "Green", "Greedy", False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 3, "F", "Light Brown", "Playful", True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 90, "F", "White", "Ethereal", True);
